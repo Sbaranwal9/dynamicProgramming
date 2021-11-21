9febd91c3930933fabaf0e7e73d5e2120baf3194
Sbaranwal9 <93540042+Sbaranwal9@users.noreply.github.com>
2021-11-21 21:22:55 +0530

    fib chnages

--- fib_mem.js
+++ fib_mem.js
@@ -1,8 +1,8 @@
 //memoization
 
 const fib = (n, m=new Map()) => {
+    if(m.has(n)) return m.get(n);
     if (n <= 2) return 1;
-    if(!m.has(n))
-        m.set(n,fib(n - 1,m) + fib(n - 2,m));
+    m.set(n,fib(n - 1,m) + fib(n - 2,m));
     return m.get(n);
 }
