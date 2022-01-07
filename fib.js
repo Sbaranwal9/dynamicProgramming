bb1aa5cdf3723df5f1451713f4e3a50f9cea6731
Sbaranwal9 <93540042+Sbaranwal9@users.noreply.github.com>
2022-01-07 09:25:59 +0530

    fib_tabulation

--- fib.js
+++ fib.js
@@ -1,4 +1,4 @@
-//memoization
+// memoization
 
 const fib = (n, m=new Map()) => {
     if(m.has(n)) return m.get(n);
@@ -6,3 +6,25 @@ const fib = (n, m=new Map()) => {
     m.set(n,fib(n - 1,m) + fib(n - 2,m));
     return m.get(n);
 }
+
+
+// tabulation
+
+const fibTab = (n) => {
+    const a = Array(n+1).fill(0);
+    a[1] = 1;
+    for(let i = 2; i <= n; i++){
+        a[i] = a[i-1]+a[i-2];
+    }
+    return a[n];
+}
+
+fib(6);
+fib(7);
+fib(8);
+fib(50);
+
+fibTab(6);
+fibTab(7);
+fibTab(8);
+fibTab(50);
