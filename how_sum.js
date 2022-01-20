5b55be92f63832c86116533a1319ca189c4e4863
Sbaranwal9 <93540042+Sbaranwal9@users.noreply.github.com>
2022-01-21 03:39:25 +0530

    how sum tab

--- how_sum.js
+++ how_sum.js
@@ -14,3 +14,31 @@ const howSum = (targetNum, numbers, m = new Map()) => {
     m.set(targetNum, null);
     return null;
 }
+
+const howSumTab = (targetNum, numbers) => {
+    const a = Array(targetNum + 1).fill(null);
+    a[0] = [];
+    for(let i = 0; i <= targetNum; i++){
+        if(a[i] !== null){
+            for(let j of numbers){
+                if(i + j <= targetNum){
+                    a[i+j] = [...a[i], j];
+                }
+            }
+        }
+    }
+    return a[targetNum];
+}
+
+
+console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2, 4]));
+console.log(howSum(8, [2, 3, 5]));
+console.log(howSum(300, [7, 14]));
+
+console.log(howSumTab(7, [2, 3]));
+console.log(howSumTab(7, [5, 3, 4, 7]));
+console.log(howSumTab(7, [2, 4]));
+console.log(howSumTab(8, [2, 3, 5]));
+console.log(howSumTab(300, [7, 14]));
