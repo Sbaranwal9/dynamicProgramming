087e00c13a505cf2cab826ae4ac568ba98503c56
Sbaranwal9 <93540042+Sbaranwal9@users.noreply.github.com>
2022-01-23 04:26:12 +0530

    canconstruct tab

--- can_construct.js
+++ can_construct.js
@@ -14,7 +14,32 @@ const canConstruct = (target, wordBank, m = new Map()) => {
     return false;
 }
 
-canConstruct("abcdef",["ab", "abc", "cd", "def", "abcd"])
-canConstruct("skateboard",["bo", "rd", "ate", "t", "ska", "sk", "boar"])
-canConstruct("enterapotentpot",["a", "p", "ent", "enter", "ot", "o", "t"])
-canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])
+const canConstructTab = (target, wordBank) => {
+    const a = Array(target.length + 1).fill(false);
+    a[0] = true;
+    for(let i = 0; i <= target.length; i++){
+        if(a[i] === true){
+            for(let word of wordBank){
+                if(i + word.length <= target.length && target.slice(i, i + word.length) === word){
+                    a[i + word.length] = true;
+                }
+            }
+        }
+    }
+    console.log(a)
+    return a[target.length];
+}
+
+console.log(canConstruct("abcdef",["ab", "abc", "cd", "def", "abcd"]))
+console.log(canConstruct("skateboard",["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+console.log(canConstruct("enterapotentpot",["a", "p", "ent", "enter", "ot", "o", "t"]))
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]))
+// true
+// false
+// true
+// false
+
+console.log(canConstructTab("abcdef",["ab", "abc", "cd", "def", "abcd"]))
+console.log(canConstructTab("skateboard",["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+console.log(canConstructTab("enterapotentpot",["a", "p", "ent", "enter", "ot", "o", "t"]))
+console.log(canConstructTab("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]))
