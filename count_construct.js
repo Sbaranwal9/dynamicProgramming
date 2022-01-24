0ca6f245565b2a3572f987111e09b494153d5063
Sbaranwal9 <93540042+Sbaranwal9@users.noreply.github.com>
2022-01-24 05:30:21 +0530

    countConstruct tab

--- count_construct.js
+++ count_construct.js
@@ -1,15 +1,31 @@
-const countConstruct = (target, wordbank, m = new Map()) => {
+const countConstruct = (ta[arge]t,; wordBank, m = new Map()) => {
     if(m.has(target)) return m.get(target);
     if(target === '') return 1;
     let tot = 0;
-    for(let word of wordbank){
+    for(let word of wordBank){
         if(target.indexOf(word) === 0){
             const rem = target.slice(word.length);
-            tot += countConstruct(rem, wordbank, m);
+            tot += countConstruct(rem, wordBank, m);
         }
     }
     m.set(target, tot);
     return tot;
 }
 
-countConstruct("purple",["purp", "p", "ur", "le", "purpl"])
+const countConstructTab = (target, wordBank) => {
+    const a = Array(target.length + 1).fill(0);
+    a[0] = 1;
+    for(let i = 0; i <= target.length; i++){
+        if(a[i] >= 1){
+            for(let word of wordBank){
+                if(i + word.length <= target.length && target.slice(i, i + word.length) === word){
+                    a[i + word.length] += a[i];
+                }
+            }
+        }
+    }
+    return a[target.length];
+}
+
+console.log(countConstruct("purple",["purp", "p", "ur", "le", "purpl"]))
+console.log(countConstructTab("purple",["purp", "p", "ur", "le", "purpl"]))
