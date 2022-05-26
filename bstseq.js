73053ddf78203307ee2657c18a719ea500c81b23
Shubham Baranwal <41940290+Sbwal@users.noreply.github.com>
2022-05-26 13:19:05 +0000

    added changes through codespacee

--- bstseq.js
+++ bstseq.js
@@ -30,47 +30,59 @@ class Node {
   const a = new Node(3);
   const b = new Node(11);
   const c = new Node(4);
-  const d = new Node(4);
-  const e = new Node(-2);
-  const f = new Node(1);
-  
   a.left = b;
   a.right = c;
-  b.left = d;
-  b.right = e;
-  c.right = f;
   
   //       3
   //    /    \
   //   11     4
-  //  / \      \
-  // 4   -2     1
   
-  bstseq(a); 
+  console.log(bstseq(a)); 
+
+  // const a = new Node(3);
+  // const b = new Node(11);
+  // const c = new Node(4);
+  // const d = new Node(4);
+  // const e = new Node(-2);
+  // const f = new Node(1);
   
-  const a = new Node(1);
-  const b = new Node(6);
-  const c = new Node(0);
-  const d = new Node(3);
-  const e = new Node(-6);
-  const f = new Node(2);
-  const g = new Node(2);
-  const h = new Node(2);
+  // a.left = b;
+  // a.right = c;
+  // b.left = d;
+  // b.right = e;
+  // c.right = f;
   
-  a.left = b;
-  a.right = c;
-  b.left = d;
-  b.right = e;
-  c.right = f;
-  e.left = g;
-  f.right = h;
+  // //       3
+  // //    /    \
+  // //   11     4
+  // //  / \      \
+  // // 4   -2     1
   
-  //      1
-  //    /   \
-  //   6     0
-  //  / \     \
-  // 3   -6    2
-  //    /       \
-  //   2         2
+  // bstseq(a); 
   
-  treeSum(a); 
+  // // const a = new Node(1);
+  // // const b = new Node(6);
+  // // const c = new Node(0);
+  // // const d = new Node(3);
+  // // const e = new Node(-6);
+  // // const f = new Node(2);
+  // // const g = new Node(2);
+  // // const h = new Node(2);
+  
+  // // a.left = b;
+  // // a.right = c;
+  // // b.left = d;
+  // // b.right = e;
+  // // c.right = f;
+  // // e.left = g;
+  // // f.right = h;
+  
+  // // //      1
+  // // //    /   \
+  // // //   6     0
+  // // //  / \     \
+  // // // 3   -6    2
+  // // //    /       \
+  // // //   2         2
+  
+  // // bstseq(a); 
