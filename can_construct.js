80a65ab9bd3ce57e696b4e1a25351a1c03b82003
Sbaranwal9 <93540042+Sbaranwal9@users.noreply.github.com>
2022-06-17 01:45:12 +0530

    leetcode

--- can_construct.js
+++ can_construct.js
@@ -14,6 +14,18 @@ const canConstruct = (target, wordBank, m = new Map()) => {
     return false;
 }
 
+var wordBreak = function(s, wordDict) {
+    const dp = Array(s.length + 1).fill(false);
+    dp[s.length] = true;
+    for(let i = s.length-1; i >= 0; i--){
+            for(let word of wordDict){
+                if(i+word.length <= s.length && word === s.slice(i, i+word.length)) dp[i] = dp[i+word.length];
+                if(dp[i]) break
+            }
+    }
+    return dp[0]
+};
+
 const canConstructTab = (target, wordBank) => {
     const a = Array(target.length + 1).fill(false);
     a[0] = true;
